53bbdd58fa2abeee3f057c137f30bdce47104b7a
Siak Hooi <siakhooi@gmail.com>
2022-06-07 20:09:58 +0800

    close #1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,3 @@
 # Change Log
+
+- `#1`, As user, I want to translate integer number 0 to 9 (simplified) enhancement
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export function numberToChineseWords(number: number): string {
-  return String(number);
+  const c = '零一二三四五六七八九';
+  if (number < 0 || number > 9) throw 'Not Supported';
+
+  return c.charAt(number);
 }
