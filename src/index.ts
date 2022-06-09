ce7de9615832ae3744ce97b2f75c1001b89d27ab
Siak Hooi <siakhooi@gmail.com>
2022-06-09 14:38:35 +0800

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,19 @@ function getDigit1(number: number): string {
   const c = '零一二三四五六七八九';
   return c.charAt(number);
 }
-export function numberToChineseWords(number: number): string {
-  if (number < 0 || number > 99) throw 'Not Supported';
-  if (number !== (number | 0)) throw 'Not Supported';
-  const c10 = '十';
-
+function getDigit2(number: number): string {
   const digit10 = (number / 10) | 0;
   const digit01 = number % 10 | 0;
-  const char10 = digit10 === 0 ? '' : getDigit1(digit10) + c10;
+  const c10 = '十';
+
+  const char10 = getDigit1(digit10) + c10;
   const char01 = digit10 > 0 && digit01 === 0 ? '' : getDigit1(digit01);
 
   return char10 + char01;
+}
+export function numberToChineseWords(number: number): string {
+  if (number < 0 || number > 99) throw 'Not Supported';
+  if (number !== (number | 0)) throw 'Not Supported';
+
+  return number < 10 ? getDigit1(number) : getDigit2(number);
 }
