ee6e7ed2efa02419b6598b254784e9434f85f62c
Siak Hooi <siakhooi@gmail.com>
2022-06-17 22:00:24 +0800

    housekeep test 0-9999

--- src/test/positive.test.ts
+++ src/test/positive.test.ts
@@ -1,7 +1,6 @@
 import {convertNumber} from '../main/index';
 
 test.each([
-  [100, '正一百'],
   [1_0000, '正一万'],
   [1_0000_0000, '正一亿'],
   [9999_9999_9999, '正九千九百九十九亿九千九百九十九万九千九百九十九'],
@@ -14,8 +13,6 @@ test.each([
   expect(convertNumber(input, {displayPositive: true})).toBe(expected);
 });
 test.each([
-  [100, '正一百'],
-  [1001, '正一千零一'],
   [1_0000, '正一萬'],
   [2_0011, '正二萬零一十一'],
   [1_0205_5070, '正一億零二百零五萬五千零七十'],
